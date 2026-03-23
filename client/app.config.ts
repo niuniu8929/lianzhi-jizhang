67b9177f5c6cbdbfa8d30660168ad52108bd2960
niuniu8929 <506627793@qq.com>
2026-03-23 21:53:06 +0800

    Update app.config.ts

--- client/app.config.ts
+++ client/app.config.ts
@@ -42,12 +42,6 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         }
       ] : 'expo-router',
       [
-        "expo-splash-screen",
-        {
-          "backgroundColor": "#ffffff"
-        }
-      ],
-      [
         "expo-image-picker",
         {
           "photosPermission": "允许联智计工App访问您的相册，以便您上传或保存图片。",
